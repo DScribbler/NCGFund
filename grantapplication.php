2578a1b913cb1f3b80a9fbb8a1a0f46479f20ee2
Damian Joshua <147677171+DScribbler@users.noreply.github.com>
2025-07-15 16:21:55 +0100

    Update grantapplication.php

--- grantapplication.php
+++ grantapplication.php
@@ -1,8 +1,7 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) session_start();
 
-$conn = new mysqli("localhost", "root", "", "nelfund_db");
-if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+include 'db_connect.php';
 
 $errors = [];
 $submitted = false;
